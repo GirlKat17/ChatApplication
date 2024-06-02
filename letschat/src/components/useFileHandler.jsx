b42eba4c5148cc1d501713ee314eec49ac2b6bbe
Katlego Patience Ramoroa <125869596+GirlKat17@users.noreply.github.com>
2024-06-02 18:29:52 +0200

    Create useFileHandler.jsx

---
+++ letschat/src/components/useFileHandler.jsx
@@ -0,0 +1,60 @@
+import { useState, useCallback } from 'react';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { app } from './firebaseConfig';
+
+export const useFileHandler = () => {
+  const [file, setFile] = useState(null);
+  const [uploadProgress, setUploadProgress] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
+
+  const storage = getStorage(app);
+
+  const handleFileChange = useCallback((e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImagePreview(reader.result);
+    };
+    reader.readAsDataURL(selectedFile);
+  }, []);
+
+  const handleUpload = useCallback(async (setImage) => {
+    if (!file) {
+      console.error('No file selected.');
+      return;
+    }
+
+    const storageRef = ref(storage, `images/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(progress);
+      },
+      (error) => {
+        console.error('Error uploading file:', error.message);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log('File available at', downloadURL);
+          setFile(null);
+          setImage(downloadURL);
+          setImagePreview(null);
+          document.getElementById('my_modal_3').close();
+        });
+      }
+    );
+  }, [file]);
+
+  return {
+    file,
+    uploadProgress,
+    imagePreview,
+    handleFileChange,
+    handleUpload,
+  };
+};
