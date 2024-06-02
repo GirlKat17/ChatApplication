6f782509cb179768a5de002d9890efb26f69a668
Katlego Patience Ramoroa <125869596+GirlKat17@users.noreply.github.com>
2024-06-02 18:29:09 +0200

    updated

--- letschat/src/components/home.jsx
+++ letschat/src/components/home.jsx
@@ -1,8 +1,20 @@
-import { useSession, signIn, signOut } from 'next';
+import { useState } from 'react';
+import AuthPage from "./auth";
 
-const LandingPage = () => {
+const LandingPage = ({ onStart }) => {
+  // You might want to use useState here if you want to track any local state
 
-  
+  return (
+    <div className="landingpage">
+      <p>Connect with friends easily & quickly</p>
+
+      <div> 
+        <button onClick={onStart}> 
+          To Start Chatting Login or Sign up
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
