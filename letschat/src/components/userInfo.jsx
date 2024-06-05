eb1c95e6faba29df9fad6944a3f67c2d39c4539e
Katlego Patience Ramoroa <125869596+GirlKat17@users.noreply.github.com>
2024-06-05 17:19:13 +0200

    profilepic to avatarurl

--- letschat/src/components/userInfo.jsx
+++ letschat/src/components/userInfo.jsx
@@ -1,34 +1,28 @@
 import React from 'react';
 
-function ProfilePic({ url }) {
-  return (
-    <div className="w-12 h-12 rounded-full overflow-hidden">
-      <img className="w-full h-full object-cover" src={url} alt="Profile Picture" />
-    </div>
-  );
-}
 
-function UsersCard({ profilepic, name, latestMessage, time, type }) {
+function UsersCard({ avatarUrl, name, latestMessage, time, type }) {
   return (
     <div className="flex items-center p-4 border-b border-gray-200 relative hover:cursor-pointer">
-      {/* Profile Picture */}
       <div className="flex-shrink-0 mr-4 relative">
-        <ProfilePic url={profilepic} />
+        <div className="w-12 h-12 rounded-full overflow-hidden">
+          <img className="w-full h-full object-cover" src={avatarUrl} alt="Avatar" />
+        </div>
       </div>
-      
-      {/* Information */}
+
       <div className="flex-1">
         <div className="flex items-center justify-between">
-          {/* Name */}
           <h2 className="text-lg font-semibold">{name}</h2>
-          {/* Time (optional) */}
           {type === "chat" && time && (
             <p className="text-sm text-gray-500">{formatTime(time)}</p>
           )}
         </div>
-        {/* Latest Message (optional) */}
-        {type === "chat" && latestMessage && (
-          <p className="text-gray-500 truncate">{latestMessage}</p>
+        {type === "user" && (
+          <div className="flex-1">
+          <div className="flex items-center justify-between">
+            <h2 className="text-lg font-semibold">{latestMessage}</h2>
+          </div>
+          </div>
         )}
       </div>
     </div>
