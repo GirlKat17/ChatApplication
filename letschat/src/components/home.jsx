03e972174d86fa76b2a06c78d7be0646a40f8b10
Katlego Patience Ramoroa <125869596+GirlKat17@users.noreply.github.com>
2024-06-05 17:17:41 +0200

    Update home.jsx

--- letschat/src/components/home.jsx
+++ letschat/src/components/home.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react';
-import AuthPage from "./auth";
+
 
 const LandingPage = ({ onStart }) => {
   // You might want to use useState here if you want to track any local state
 
   return (
     <div className="landingpage">
-      <p>Connect with friends easily & quickly</p>
+      <div className='landingtext'> Connect with friends easily & quickly</div>
 
-      <div> 
+      <div  className='landingBtn'> 
         <button onClick={onStart}> 
           To Start Chatting Login or Sign up
         </button>
