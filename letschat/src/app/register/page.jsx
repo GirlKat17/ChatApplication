980e3c1c1bc73c1c7d49475ac3908ab1ee2a9624
Katlego Patience Ramoroa <125869596+GirlKat17@users.noreply.github.com>
2024-06-07 13:36:27 +0200

    Create page.jsx

---
+++ letschat/src/app/register/page.jsx
@@ -0,0 +1,9 @@
+'use client'
+
+import UserProfile from "@/components/userProfile"; // Make sure to import it with an uppercase
+
+const Register = () => {
+    return <UserProfile />; // Use the uppercase name here as well
+};
+
+export default Register;
